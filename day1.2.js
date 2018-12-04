f1c8881a82236a985c660dfd4c550a1c401e049d
JackMcHale <jack.mchale@gmail.com>
2018-12-03 21:07:27 -0500

    removed old comments

--- day1.2.js
+++ day1.2.js
@@ -1,54 +1,47 @@
 
-function main()
-{
+function main() {
     //Get values from file
     var fs = require('fs');
- 
+
     let datafile = fs.readFileSync('data/dayone.txt', 'utf8');
-        //Parse values into array of strings
+    //Parse values into array of strings
     let valueArray = datafile.split("\n");
     //Pass values into adder
     let result = findFirst(valueArray);
-        //Print result
-    process.stdout.write(result+"");
+    //Print result
+    process.stdout.write("Final" + result + "");
 }
 
-function findFirst (inputValues)
-{
+function findFirst(inputValues) {
     var result = 0;
     var resultArray = [0];
+    var superInt = 0;
+    var foundDuplicate = false;
     //loop through values
-    for(i=0; i<inputValues.length;i++){
-        let input = inputValues[i];
-        //get first character
-        let firstChar = input.charAt(0);
-        let value = parseInt(input.substring(1),10);
-        if(firstChar == "+")
-        {
-            result += value;
-        }
-        else if(firstChar =="-")
-        {
-            result -= value;
-        }
-        process.stdout.write(result + ":" + resultArray.length + "\n");
-        process.stdout.write(resultArray.includes(result)+ "\n");
-        if(resultArray.includes(result))
-        {
-            break;
-        }
-        else
-        {
-            resultArray.push(result);
-            process.stdout.write("adding" + result + "\n");
+    while (!foundDuplicate) {// && superInt <128000) {
+        for (i = 0; i < inputValues.length; i++) {
+            let input = inputValues[i];
+            superInt++;
+            //get first character
+            let firstChar = input.charAt(0);
+            let value = parseInt(input.substring(1), 10);
+            if (firstChar == "+") {
+                result += value;
+            }
+            else if (firstChar == "-") {
+                result -= value;
+            }
+            process.stdout.write(result + "\n")
+            if (resultArray.includes(result)) {
+                foundDuplicate = true;
+                break;
+            }
+            else {
+                resultArray.push(result);
+            }
         }
     }
-
-
     return result;
-    //Add result to array
-    //If already exists
-    //return
 }
 
 main();
