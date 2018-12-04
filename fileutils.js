50c5b8e7ab44916cf54333a374e1f1483a8a8b5a
JackMcHale <jack.mchale@gmail.com>
2018-12-03 21:29:30 -0500

    Refactoring file access into a reusable library

---
+++ fileutils.js
@@ -0,0 +1,8 @@
+exports.getArrayFromFile = function (filename) {
+    //Get values from file
+    var fs = require('fs');
+
+    let datafile = fs.readFileSync('data/' + filename, 'utf8');
+    //Parse values into array of strings
+    return datafile.split("\n");
+}
