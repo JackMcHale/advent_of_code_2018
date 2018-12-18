778543e4594d7747cd121eb9f686b8b3f18e1dc6
Jack McHale <john.mchale@advance-auto.com>
2018-12-18 16:26:13 -0500

    more comments in main function

--- day4.1.js
+++ day4.1.js
@@ -1,4 +1,5 @@
 var utils = require('./fileutils.js');
+
 function main() {
     //Get values from file
     let valueArray = utils.getArrayFromFile('daythfour.txt');
@@ -11,8 +12,18 @@ function main() {
 function getGuardAndTime(inputValues) {
     var result = [];
     var guardResults = {};
-
+    var currentGuard = {};
+    var guardIsAsleep = false;
     //For each row
+    //parse record
+    //switch event type
+    //case start
+    //end shift of previous guard
+    //set current guard
+    //case sleep
+    //set starting minute
+    //case end sleep
+    //
     return result;
 }
 
@@ -22,37 +33,33 @@ function parseRecord(record) {
 
 }
 
-function startShift(gs)
-{
+function startShift(gevent, gres) {
+
+    return gres;
+}
+
+function endShift(gs) {
 
 }
 
-function endShift(gs)
-{
+function startSleep(gs) {
 
 }
 
-function startSleep(gs)
-{
+function endSleep(gs) {
 
 }
 
-function endSleep(gs)
-{
-
-}
-
-function GuardEvent(guardId,minute,type)
-{
+function GuardEvent(guardId, minute, type) {
     this.id = guardId;
     this.minute = minute;
     this.type = type;
 }
 
 var EventType = {
-    START_SHIFT:0,
-    START_SLEEP:1,
-    END_SLEEP:2
+    START_SHIFT: 0,
+    START_SLEEP: 1,
+    END_SLEEP: 2
 }
 
 function GuardStats(guardId) {
